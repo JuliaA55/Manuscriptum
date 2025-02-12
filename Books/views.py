0c2d25ac62f70566c44d524df94a3518a1e351f1
Julia <hivizion69@gmail.com>
2025-02-12 12:21:18 +0200

    Add search

--- Books/views.py
+++ Books/views.py
@@ -4,5 +4,9 @@ from .models import Book
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
+    search_query = request.GET.get('search')
+
+    if search_query:
+        books = books.filter(title__istartswith=search_query) | books.filter(title__icontains=search_query)
 
     return render(request, 'books/book_list.html', {'books': books})
